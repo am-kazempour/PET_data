e5b9d052f1c974301c3c32b3be5e6f880136cbf5
Amin Kazempour <77267939+am-kazempour@users.noreply.github.com>
2025-06-26 13:01:24 +0330

    Update kazempour.py

--- kazempour.py
+++ kazempour.py
@@ -1,1 +1,13 @@
-print('amin')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "amin"
+
+if __name__ == '__main__':
+    # Render خودش پورت رو مشخص میکنه، از متغیر محیطی بخون
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
